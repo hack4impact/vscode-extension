1f1382b4295893928273207f29470f435beb41d4
Yash Totale <totaleyash@gmail.com>
2021-02-25 13:09:17 -0800

    Updated vsce test

--- __tests__/misc/vsce.test.ts
+++ __tests__/misc/vsce.test.ts
@@ -1,21 +1,27 @@
+// Externals
+import { existsSync } from "fs";
+import { join } from "path";
 import { listFiles, PackageManager } from "vsce";
+
+// Internals
 import { checkArray } from "../helpers";
 
 test("Correct files are packaged", async () => {
   const files = await listFiles({ packageManager: PackageManager.Npm });
+  const distPath = join(__dirname, "..", "..", "dist");
 
-  checkArray(
-    [
-      "AUTHORS",
-      "CHANGELOG.md",
-      "LICENSE.md",
-      "package.json",
-      "README.md",
-      "static/hack4impact-icon.png",
-      "static/rotating-icon.gif",
-      "dist/extension.js",
-      "dist/extension.js.map",
-    ],
-    files
-  );
+  const expected = [
+    "AUTHORS",
+    "CHANGELOG.md",
+    "LICENSE.md",
+    "package.json",
+    "README.md",
+    "static/hack4impact-icon.png",
+    "static/rotating-icon.gif",
+  ];
+
+  if (existsSync(distPath))
+    expected.push("dist/extension.js", "dist/extension.js.map");
+
+  checkArray(expected, files);
 });
