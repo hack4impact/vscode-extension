fdf9e205f9c0b9e385d47007bba6ef094d138f04
Yash Totale <totaleyash@gmail.com>
2021-03-02 23:28:21 -0800

    Updated Test Coverage Bash Script

--- scripts/test-coverage.sh
+++ scripts/test-coverage.sh
@@ -4,3 +4,9 @@ set -e
 
 npm test -- --coverage
 cp static/hack4impact-icon.png coverage/lcov-report/favicon.png
+
+if [[ "$OSTYPE" == "darwin"* ]]; then
+  sed "-i" "" "-e" "s/Code coverage report for All files/Code Coverage | Hack4Impact's VS Code Extension/g" "coverage/lcov-report/index.html"
+else
+  sed "-i" "-e" "s/Code coverage report for All files/Code Coverage | Hack4Impact's VS Code Extension/g" "coverage/lcov-report/index.html"
+fi
